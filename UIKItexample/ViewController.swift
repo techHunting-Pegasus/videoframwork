836b97eb5860d8e066293e59be24fdad4479e34c
Ishpreet singh <ishpreet.s@multitvsolution.com>
2026-05-23 12:13:55 +0530

    first commit

---
+++ UIKItexample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKItexample
+//
+//  Created by Ishpreet singh on 22/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
